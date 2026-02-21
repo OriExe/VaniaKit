cacdf5f4b1e414d2b3391c40eb5d62305d05187e
Azam F <115250689+OriExe@users.noreply.github.com>
2026-02-21 16:30:52 +0000

    Progress

--- Assets/VaniaKit/Scripts/EnemyAi/PatrolEnemyAi.cs
+++ Assets/VaniaKit/Scripts/EnemyAi/PatrolEnemyAi.cs
@@ -1,13 +1,25 @@
+using System;
 using UnityEngine;
 
 namespace Vaniakit.Ai
 {
     public class PatrolEnemyAi : MonoBehaviour, IDamageable
     {
+        private int pointToFollowIndex;
+        private bool aiIdle;
         [SerializeField] private float speed = 2;
         [SerializeField] private Transform[] pointsToGoTo;
-        private int pointToFollowIndex;
         [SerializeField] private float health;
+        [Header("PLayer Detection ranges")]
+        [Tooltip("Line of Sight Distance in Yellow")]
+        [SerializeField] private float lineOfSightDistance;
+        [Tooltip("Detection Range in Red")]
+        [SerializeField] private float detectionRange;
+        [Header("Grounded Values")]
+        [SerializeField] private float groundedDetectionRange;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask groundMask;
+        protected bool isGrounded;
         #region Events
         protected virtual void onDeath()
         {
@@ -17,11 +29,41 @@ namespace Vaniakit.Ai
         {
 
         }
+        
+        protected virtual void onPlayerInLineOfSight()
+        {
+            
+        }
+
+        protected virtual void onPlayerNearby()
+        {
+            
+        }
+
+        protected virtual void onIdle()
+        {
+            
+        }
+
+        protected virtual void onReachedPatrolPoint(int index)
+        {
+            Invoke("switchPointToPatrol", 4f);
+        }
         #endregion
 
         private void Update()
         {
-            onPatrol();
+            patrolling();
+        }
+
+        void applyGravity()
+        {
+            if (groundCheck == null)
+            {
+                Debug.LogWarning("Ground Check is null Ai won't fall");
+                return;
+            }
+            transform.position = transform.up * -0.0981f * Time.deltaTime;
         }
         public void OnHit(int damage = 0, bool isCritical = false)
         {
@@ -41,7 +83,7 @@ namespace Vaniakit.Ai
             }
         }
 
-        protected virtual void onPatrol()
+        private void patrolling()
         {
             if (pointsToGoTo.Length <=1)
             {
@@ -49,14 +91,29 @@ namespace Vaniakit.Ai
                 return;
             }
 
-            if (Vector2.Distance(pointsToGoTo[pointToFollowIndex].position, transform.position) < 0.2f)
+            if (Vector2.Distance(pointsToGoTo[pointToFollowIndex].position, transform.position) < 0.1f && !aiIdle)
             {
-                pointToFollowIndex++;
-                pointToFollowIndex %= pointsToGoTo.Length;
+                onReachedPatrolPoint(pointToFollowIndex);
+                aiIdle = true;
             }
-            transform.position = Vector2.Lerp(transform.position,pointsToGoTo[pointToFollowIndex].position, speed);
+            if (!aiIdle)
+                transform.position = Vector2.MoveTowards(transform.position, pointsToGoTo[pointToFollowIndex].position, speed / 1 * Time.deltaTime);
         }
 
-      
+        protected void switchPointToPatrol()
+        {
+            pointToFollowIndex++;
+            pointToFollowIndex %= pointsToGoTo.Length;
+            aiIdle = false;
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, detectionRange);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * lineOfSightDistance);
+        }
     }
 }
