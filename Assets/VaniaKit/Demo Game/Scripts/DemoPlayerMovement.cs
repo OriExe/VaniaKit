be606edd16d633d40d2a293dbb62d6a84d126b34
Azam F <115250689+OriExe@users.noreply.github.com>
2026-05-06 00:49:30 +0100

    Tile pallet

--- Assets/VaniaKit/Demo Game/Scripts/DemoPlayerMovement.cs
+++ Assets/VaniaKit/Demo Game/Scripts/DemoPlayerMovement.cs
@@ -1,12 +1,31 @@
+using System;
 using UnityEngine;
 
 
 public class DemoPlayerMovement : Vaniakit.Player.PlayerMovement
 {
     [SerializeField] private AudioSource walkSound;
-
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer sprite;
     protected override void onPlayerMove(lookStatesHorizontal direction)
     {
         walkSound.Play();
+        animator.SetBool("isWalking", true);
+        switch (direction)
+        {
+            case lookStatesHorizontal.left:
+                sprite.flipX = true;
+                break;
+            case lookStatesHorizontal.right:
+                sprite.flipX = false;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+
+    protected override void onPlayerIdle()
+    {
+        animator.SetBool("isWalking", false);
     }
 }
