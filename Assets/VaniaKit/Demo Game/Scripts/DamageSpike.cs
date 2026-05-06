31f631613b9cefdb9815a237c3e7158f7cd63b88
Azam F <115250689+OriExe@users.noreply.github.com>
2026-05-07 00:30:26 +0100

    Made elements bigger

--- Assets/VaniaKit/Demo Game/Scripts/DamageSpike.cs
+++ Assets/VaniaKit/Demo Game/Scripts/DamageSpike.cs
@@ -4,10 +4,21 @@ using Vaniakit.Collections;
 
 public class DamageSpike : MonoBehaviour,IDamageable
 {
+    private float timeOut;
     public void OnHit(int damage = 0, bool isCritical = false, float cooldownPeriod = 0,
         IDamageable.Direction directionOfAttack = IDamageable.Direction.none)
     {
-        Vaniakit.Player.PlayerController.getPlayerRigidbody().AddForce(new Vector2(0f, 13f), ForceMode2D.Impulse);
+        if (timeOut > 1)
+        {
+            timeOut = 0;
+            Vaniakit.Player.PlayerController.getPlayerRigidbody().AddForce(new Vector2(0f, 13f), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Update()
+    {
+        timeOut += Time.deltaTime;
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
