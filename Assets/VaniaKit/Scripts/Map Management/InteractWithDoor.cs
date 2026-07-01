2e129986bb28f6b3ee8c1cafe29148e14cbe11e1
Azam F <115250689+OriExe@users.noreply.github.com>
2026-07-01 23:13:57 +0100

    Added namespace to door script

--- Assets/VaniaKit/Scripts/Map Management/InteractWithDoor.cs
+++ Assets/VaniaKit/Scripts/Map Management/InteractWithDoor.cs
@@ -1,86 +1,89 @@
 using UnityEngine;
 using Vaniakit.Collections;
-using Vaniakit.Map;
 using Vaniakit.Misc;
 using Vaniakit.ResourceManager;
 
-public class InteractWithDoor : ATeleporterMonoBehaviour,IInteractable
+namespace Vaniakit.Map
 {
-    [Tooltip("Put the key that opens the door here")]
-    [SerializeField]private InventorySlot key;
+    public class InteractWithDoor : ATeleporterMonoBehaviour,IInteractable
+    {
+        [Tooltip("Put the key that opens the door here")]
+        [SerializeField]private InventorySlot key;
 
-    [SerializeField] private string Destination;
-    [SerializeField] private string DestinationSceneName;
+        [SerializeField] private string Destination;
+        [SerializeField] private string DestinationSceneName;
 
-    [SerializeField] private string lockedMessage = "You'll need a key";
-    [SerializeField] private string notLockedMessage = "Press E to enter the door";
-    // Shows text on the player's screen
+        [SerializeField] private string lockedMessage = "You'll need a key";
+        [SerializeField] private string notLockedMessage = "Press E to enter the door";
+        // Shows text on the player's screen
 
-    #region Events
-    protected virtual void onPlayerInteractsWithKey()
-    {
-        Debug.Log("Player Interacted with key");
-    }
+        #region Events
+        protected virtual void onPlayerInteractsWithKey()
+        {
+            Debug.Log("Player Interacted with key");
+        }
 
-    protected virtual void onPlayerInteractsWithoutKey()
-    {
-        Debug.Log("Player Interacted without key");
-    }
-    protected virtual void onPlayerLoadedHere()
-    {
-        Debug.Log("Player Loaded Here");
-    }
-    #endregion
-   
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player" && Inventory.GetAllItems().Contains(key))
+        protected virtual void onPlayerInteractsWithoutKey()
         {
-            ShowTextTool.showText(notLockedMessage);
+            Debug.Log("Player Interacted without key");
         }
-        else if (other.tag == "Player")
+        protected virtual void onPlayerLoadedHere()
         {
-            ShowTextTool.showText(lockedMessage);
+            Debug.Log("Player Loaded Here");
         }
-    }
-    //Hides text from the player screen
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Player")
+        #endregion
+       
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            ShowTextTool.hideText();
+            if (other.tag == "Player" && Inventory.GetAllItems().Contains(key))
+            {
+                ShowTextTool.showText(notLockedMessage);
+            }
+            else if (other.tag == "Player")
+            {
+                ShowTextTool.showText(lockedMessage);
+            }
         }
-    }
+        //Hides text from the player screen
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                ShowTextTool.hideText();
+            }
+        }
 
-    /// <summary>
-    /// If the player has the correct key the door will open
-    /// </summary>
-    public void onInteract()
-    {
-        if (Inventory.GetAllItems().Contains(key))
+        /// <summary>
+        /// If the player has the correct key the door will open
+        /// </summary>
+        public void onInteract()
         {
-            onPlayerInteractsWithKey();
-            FadeInManager.instance.StartCoroutine(FadeInManager.instance.FadeToBlack(DestinationSceneName, Destination));
+            if (Inventory.GetAllItems().Contains(key))
+            {
+                onPlayerInteractsWithKey();
+                FadeInManager.instance.StartCoroutine(FadeInManager.instance.FadeToBlack(DestinationSceneName, Destination));
+            }
+            else
+            {
+                onPlayerInteractsWithoutKey();
+            }
         }
-        else
-        {
-            onPlayerInteractsWithoutKey();
-        }
-    }
 
-    /// <summary>
-    /// Runs when someone teleports at the door 
-    /// </summary>
-    /// <param name="gameObjectName"></param>
-    /// <returns></returns>
-    public override bool amITheRightObject(string gameObjectName)
-    {
-        if (gameObjectName == gameObject.name)
+        /// <summary>
+        /// Runs when someone teleports at the door 
+        /// </summary>
+        /// <param name="gameObjectName"></param>
+        /// <returns></returns>
+        public override bool amITheRightObject(string gameObjectName)
         {
-            onPlayerLoadedHere();
-            //justTeleported = true;
-            return true;
+            if (gameObjectName == gameObject.name)
+            {
+                onPlayerLoadedHere();
+                //justTeleported = true;
+                return true;
+            }
+            return false;
         }
-        return false;
     }
 }
+
